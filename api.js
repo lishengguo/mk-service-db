50b00aed33ec3517268c1d72c6998d21680f9ac9
lishengguo <lishengguo@qq.com>
2017-08-29 17:55:06 +0800

    更新api导出

--- api.js
+++ api.js
@@ -1,8 +1,9 @@
 const Sequelize = require("sequelize")
 const cls = require('continuation-local-storage')
 Sequelize.useCLS(cls.createNamespace('my-own-namespace'))
+ 
+let current = null
 
-var config
 const dbs = {}
 
 const api = {
@@ -11,22 +12,22 @@ const api = {
 
     getDB: (name) => dbs[name || 'currentDB'],
 
-    _init: (current) => {
-        config = current
-        dbs.currentDB = dbs[config.name] = newDB(config)
-        dbs.currentDB.config.transactionType = config.transactionType
-        if (Array.isArray(config.dbs)) {
-            config.dbs.filter(d => !dbs[d.name]).forEach(d => {
+    _init: (options) => { 
+        current = options
+        dbs.currentDB = dbs[current.name] = newDB(current)
+        dbs.currentDB.config.transactionType = current.transactionType
+        if (Array.isArray(current.dbs)) {
+            current.dbs.filter(d => !dbs[d.name]).forEach(d => {
                 dbs[d.name] = newDB(d);
                 dbs[d.name].config.transactionType = d.transactionType
             })
         }
-        if (config.server) { //注册数据库事务拦截器
-            var array = config.server.interceptors || [];
+        if (current.server) { //注册数据库事务拦截器
+            var array = current.server.interceptors || [];
             if (array.filter(a => a == interceptor) == 0) {
                 array.push(interceptor)
             }
-            config.server.interceptors = array
+            current.server.interceptors = array
         }
     }
 }
@@ -47,8 +48,8 @@ function interceptor(ctx) {
         || serviceConfig && serviceConfig.db && serviceConfig.db.config.transactionType;
     if (serviceConfig && serviceConfig.db) {
         currentDB = serviceConfig.db
-    }
-    transactionType = transactionType || config.transactionType
+    } 
+    transactionType = transactionType || current.transactionType
     currentDB = currentDB || dbs.currentDB
 
     if (currentDB && transactionType == 'auto' && !ctx._handler) {
@@ -65,4 +66,30 @@ function interceptor(ctx) {
     return true
 }
 
-module.exports = api
+
+
+/*
+ *  config.js        //初始化参数设置
+*/
+
+function config(options) { 
+    let current = config.current
+    Object.assign(current, options)
+    if (!options.host && options.dbs) {
+        Object.assign(current, current.dbs[0])
+    }
+    api._init(current)
+}
+config.current = {
+    name: "bizdata",
+    type: "mysql",
+    user: "root",
+    pwd: "mydbpassword",
+    host: "localhost",
+    port: 3306,
+    database: "bizdata_dev",
+    transactionType: "auto",
+    dbs: [],
+}
+
+module.exports = { api, config }
